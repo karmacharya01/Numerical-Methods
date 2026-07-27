5196795e7d6cdeb1bd9467c9537163c7400880b2
karmacharya01 <karmacharyakritish24@gmail.com>
2026-07-27 20:31:36 +0545

    Move existing files into my-folder

---
+++ Solution_of_Non-Linear_Equations/FalsiPointMethod.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <math.h>
+
+ float Equation(float x)
+{
+    return pow(x, 2) - 10; // Equation goes here
+}
+
+int main()
+{
+    float a, b, error, midpoint = 0;
+    int i = 1, maxIteration;
+
+    while (1)
+    {
+        printf("Enter the value of a: ");
+        scanf("%f", &a);
+
+        printf("Enter the value of b: ");
+        scanf("%f", &b);
+
+        if (Equation(a) * Equation(b) < 0)
+            break;
+
+        printf("The root doesn't lie within this interval.\n");
+    }
+
+    printf("Enter the tolerable error: ");
+    scanf("%f", &error);
+
+    printf("Enter the maximum iterations: ");
+    scanf("%d", &maxIteration);
+
+    do
+    {
+        midpoint = (a * Equation(b) - b * Equation(a)) / (Equation(b) - Equation(a));
+
+        printf("Iteration %d : %.6f\n", i, midpoint);
+
+        if (Equation(a) * Equation(midpoint) < 0)
+            b = midpoint;
+        else
+            a = midpoint;
+
+        i++;
+    } while ((fabs(b - a) / 2 >= error) && (i <= maxIteration));
+
+    printf("\nThe approximate root is %.6f\n", midpoint);
+
+    return 0;
+}
+
+// Algorithm
+
+// Step 1 : START
+// Step 2 : Define the function for given Equation.
+// Step 3 : Input interval bounds a and b, tolerable error, and maximum iterations.
+// Step 4 : If (Equation(a) times Equation(b) >= 0, display "The root doesn't lie within this interval" and repeat Step 3.
+// Step 5 : Calcualte the midpoint (a * Equation(b) - b * Equation(a)) / (Equation(b) - Equation(a))
+// Step 6 : Print the current iteration number and the midpoint
+// Step 7 : if (Equation(a) * Equation(midpoint) < 0) set b = midpoint Otherwise a = midpoint
+// Step 8 : Repeat the step 5 to 7 while ((fabs(b - a) / 2 >= error) && (i <= maxIteration))
+// Step 9 : Print the final approximate root.
+// Step 10 : STOP
